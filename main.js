6ebab19d673984e1b7755d63a783ab887442c668
mmpotulo28 <mpotulom28@gmail.com>
2023-08-28 18:11:23 +0200

    add theme to local storage

--- main.js
+++ main.js
@@ -1,32 +1,25 @@
-/****Theme picker****/
-const themeOpt = document.querySelector("#themeOpt")
+//reference to the HTML elements
+const themeRadio = document.querySelectorAll('input[name="theme"]');
 
-themeOpt.onchange = function theme() {
+//add an event lister to each radio button
+themeRadio.forEach(radio => radio.addEventListener('click', setTheme));
 
-    if (themeOpt.value == "gg") {
-        document.body.classList.remove("GP-theme")
-        document.body.classList.remove("BP-theme")
-        document.body.classList.remove("BG-theme")
-        document.body.classList.add("GG-theme")
-        console.log("theme GG");
-    } else if (themeOpt.value == "gp") {
-        document.body.classList.remove("GG-theme")
-        document.body.classList.remove("BP-theme")
-        document.body.classList.remove("BG-theme")
-        document.body.classList.add("GP-theme")
-        console.log("theme GP");
-    } else if (themeOpt.value == "bg") {
-        document.body.classList.remove("GG-theme")
-        document.body.classList.remove("GP-theme")
-        document.body.classList.remove("BP-theme")
-        document.body.classList.add("BG-theme")
-        console.log("theme BG");
-    } else {
-        document.body.classList.remove("GG-theme")
-        document.body.classList.remove("GP-theme")
-        document.body.classList.remove("BG-theme")
-        document.body.classList.add("BP-theme")
-        console.log("theme BP");
-    }
-
+//store the theme
+function setTheme(e) {
+    localStorage.setItem('theme', e.target.id);
 }
+
+//load the theme when the page loads
+document.onload = loadTheme();
+
+//restore the theme
+function loadTheme() {
+    const theme = localStorage.getItem('theme');
+    if (theme) {
+        document.documentElement.className = theme;
+        document.getElementById(theme).checked = true;
+    } else{
+        console.log("No theme on storage!");
+    }
+}
+
