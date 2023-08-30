d08985e7ce743d6fe8e03d93bfefe1724cd25c29
mmpotulo28 <mpotulom28@gmail.com>
2023-08-30 10:03:01 +0200

    add async counters

--- v.3.0/assets/js/gates.js
+++ v.3.0/assets/js/gates.js
@@ -67,3 +67,36 @@ export function norGate(in1, in2) {
     out = notGate(orGate(in1, in2))
     return out
 }
+
+/************function for a clock***********/
+let clk = 0, clkC = 1, edge = 0, timer = 0
+export function clock() {
+
+    //assign prev clock value to the temp variable
+    var temp = clk
+
+    //change clock per second
+    if (timer % 60 == 0) {
+        clkC *= -1
+
+        if (clkC === 1) {
+            clk = 0;
+        } else if (clkC === -1) {
+            clk = 1;
+        }
+    }
+
+    //compare current clock with prev clock (temp)
+    if (clk != temp && temp == 0) {
+        edge = 1
+    } else if (clk === temp) {
+        edge = 0
+    }
+
+
+    //if clock and temp are not the same then that is an edge
+    //temp will change with less than a second which is good
+
+    timer++
+    return edge
+}
