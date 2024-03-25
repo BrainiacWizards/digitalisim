a5d4230c6d97ca11499e00a9b88e101db9dc05c8
mmpotulo28 <mpotulom28@gmail.com>
2024-03-25 21:09:56 +0200

    fix login and register authentication issue

--- public/v.3.0/assets/js/login.js
+++ public/v.3.0/assets/js/login.js
@@ -1,83 +1,158 @@
-import { initializeApp } from "firebase/app";
-import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
-import { getDatabase } from "firebase/database";
+import {
+ getApp,
+ initializeApp,
+} from 'https://www.gstatic.com/firebasejs/9.6.1/firebase-app.js';
 
-const login_form = document.querySelector( ".login_form" );
-const login_user = document.querySelector( "#login_user" );
-const login_pass = document.querySelector( "#login_pass" );
-const login_status = document.querySelector( "#login_status" );
+import {
+ getAuth,
+ createUserWithEmailAndPassword,
+ signInWithEmailAndPassword,
+} from 'https://www.gstatic.com/firebasejs/9.6.1/firebase-auth.js';
+
+import { getDatabase } from 'https://www.gstatic.com/firebasejs/9.6.1/firebase-database.js';
+
+const login_form = document.querySelector('.login_form');
+const login_user = document.querySelector('#login_user');
+const login_pass = document.querySelector('#login_pass');
+const login_status = document.querySelector('#login_status');
+const registerBtn = document.querySelector('#register-btn');
 
 const firebaseConfig = {
-    apiKey: "AIzaSyB1PW1bEv2Ks4FZFWGYJ8btpKelaLpFTDg",
-    authDomain: "digitalisim.firebaseapp.com",
-    projectId: "digitalisim",
-    storageBucket: "digitalisim.appspot.com",
-    messagingSenderId: "236908555465",
-    appId: "1:236908555465:web:133c7b9bd5f436e00bb118",
-    measurementId: "G-TY251NQVE1"
+ apiKey: 'AIzaSyB1PW1bEv2Ks4FZFWGYJ8btpKelaLpFTDg',
+ authDomain: 'digitalisim.firebaseapp.com',
+ projectId: 'digitalisim',
+ storageBucket: 'digitalisim.appspot.com',
+ messagingSenderId: '236908555465',
+ appId: '1:236908555465:web:133c7b9bd5f436e00bb118',
+ measurementId: 'G-TY251NQVE1',
 };
 
-const app = initializeApp( firebaseConfig );
-const database = getDatabase( app );
+const app = initializeApp(firebaseConfig);
 const auth = getAuth();
+const Database = getDatabase(app);
 
-login_form.addEventListener( "submit", function ( e ) {
-    e.preventDefault();
-    console.log('submit')
-    register();
-} );
+const rdUrl = new URL(window.location.href);
+const rdf = rdUrl.searchParams.get('rdf');
 
-
-function register () {
-    const username = login_user;
-    const password = login_pass;
-    const auth = getAuth();
-
-    if ( !validateEmail( username ) || !validatePassword( password ) ) {
-        login_status.innerHTML = "Invalid email or password";
-        return;
-    }
-
-    createUserWithEmailAndPassword( auth, email, password )
-        .then( ( userCredential ) => {
-            // Signed up 
-            const user = userCredential.user;
-            var ref = database.ref( 'users/' + user.uid );
-
-            const userData = {
-                email: user.email,
-                uid: user.uid,
-                last_login: Date.now()
-            };
-
-            ref.set( userData );
-
-            login_status.innerHTML = "User created";
-            alert( "User created" );
-        } )
-        .catch( ( error ) => {
-            const errorCode = error.code;
-            const errorMessage = error.message;
-            login_status.innerHTML = errorMessage;
-        } );
+// check if user is already logged in
+if (!sessionStorage.getItem('loggedIn') === 'true') {
+ if (rdf) {
+  window.location.href = rdf;
+ } else {
+  window.location.href = '../../';
+ }
 }
 
-function validatePassword ( pass ) {
-    if ( pass.length < 8 ) {
-        login_status.innerHTML = "invalid password";
-        return false;
-    }
+login_form.addEventListener('submit', function (e) {
+ e.preventDefault();
+ console.log('submit');
+ login();
+});
 
-    return true;
+registerBtn.addEventListener('click', function (e) {
+ e.preventDefault();
+ register();
+});
+
+async function login() {
+ console.log('logging in');
+ const username = login_user.value;
+ const password = login_pass.value;
+
+ if (!validateEmail(username) || !validatePassword(password)) {
+  return;
+ }
+
+ try {
+  await signInWithEmailAndPassword(auth, username, password);
+  login_status.innerHTML = 'Logged in successfully';
+  login_status.style.color = 'green';
+
+  setLoginSessionCookie(username);
+ } catch (error) {
+  login_status.innerHTML = error.message;
+  login_status.style.color = 'red';
+  login_pass.value = '';
+
+  if (error.code === 'auth/invalid-login-credentials') {
+   login_status.innerHTML = 'Invalid email or password';
+   throw new Error('Invalid email or password');
+  }
+
+  login_status.innerHTML = 'An error occurred';
+  throw new Error(`An error occurred:\n${error}`);
+ }
 }
 
-function validateEmail ( email ) {
-    const emailExpression = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+async function register() {
+ console.log('registering');
+ const username = login_user.value;
+ const password = login_pass.value;
 
-    if ( !emailExpression.test( email ) ) {
-        login_status.innerHTML = "Invalid email";
-        return false;
-    }
+ if (!validateEmail(username) || !validatePassword(password)) {
+  //   login_status.innerHTML = 'Invalid email or password';
+  return;
+ }
 
-    return true;
+ try {
+  await createUserWithEmailAndPassword(auth, username, password);
+  login_status.innerHTML = 'User created successfully';
+  login_status.style.color = 'green';
+ } catch (error) {
+  login_status.innerHTML = error.message;
+  login_status.style.color = 'red';
+  login_pass.value = '';
+
+  if (error.code === 'auth/email-already-in-use') {
+   login_status.innerHTML = 'Email already in use';
+   throw new Error('Email already in use');
+  }
+
+  if (error.code === 'auth/weak-password') {
+   login_status.innerHTML = 'Password is too weak';
+   throw new Error('Password is too weak');
+  }
+
+  if (error.code === 'auth/invalid-email') {
+   login_status.innerHTML = 'Invalid email';
+   throw new Error('Invalid email');
+  }
+
+  login_status.innerHTML = 'An error occurred';
+  throw new Error('An error occurred');
+ }
+}
+
+function validatePassword(pass) {
+ if (pass.length < 8) {
+  login_status.innerHTML = 'invalid password';
+  login_status.style.color = 'red';
+  return false;
+ }
+
+ return true;
+}
+
+function validateEmail(email) {
+ const emailExpression = /^[a-zA-Z0-9._%+-]+@gmail\.com$/;
+
+ if (!emailExpression.test(email)) {
+  login_status.innerHTML = 'Invalid email';
+  login_status.style.color = 'red';
+  return false;
+ }
+
+ return true;
+}
+
+function setLoginSessionCookie(email) {
+ sessionStorage.setItem('loggedIn', 'true');
+
+ setTimeout(() => {
+  if (rdf) {
+   window.location.href = rdf;
+  } else {
+   window.location.href = '../../';
+  }
+ }, 500);
 }
